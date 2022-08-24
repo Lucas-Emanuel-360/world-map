7d1e57923b2fa0225bad801221e50ffb29954f4d
Lucas Emanuel <68708665+Lucas-Emanuel-360@users.noreply.github.com>
2022-08-24 09:36:15 -0300

    Initial Files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    var ImageMap = function (map, img) {
+        var n,
+            areas = map.getElementsByTagName('area'),
+            len = areas.length,
+            coords = [],
+            previousWidth = 2000;
+        for (n = 0; n < len; n++) {
+            coords[n] = areas[n].coords.split(',');
+        }
+        this.resize = function () {
+            var n, m, clen,
+                x = img.offsetWidth / previousWidth;
+            for (n = 0; n < len; n++) {
+                clen = coords[n].length;
+                for (m = 0; m < clen; m++) {
+                    coords[n][m] *= x;
+                }
+                areas[n].coords = coords[n].join(',');
+            }
+            previousWidth = img.offsetWidth;
+            return true;
+        };
+        window.onresize = this.resize;
+    },
+        imageMap = new ImageMap(document.getElementById('mapa'), document.getElementById('img'));
+    imageMap.resize();
+    return;
+}
+
+function identificarArea(nomeArea) {
+    document.getElementById('areaDestaque')
+    .innerHTML = nomeArea;
+}
