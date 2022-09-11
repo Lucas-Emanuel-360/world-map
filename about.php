0c95584bcfc618491b3ea50ad97da6e065be23c1
Lucas-Emanuel-360 <emanuellucas72902@gmail.com>
2022-09-11 17:26:14 -0300

    about us page

---
+++ about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/style-about.css">
+    <script src="public/js/script-theme.js"></script>
+    <title>Sobre Nos</title>
+</head>
+<body>
+<header>
+
+<a class="text" href="index.html">Home</a>
+<a class="text" href="about.php">Sobre Nós</a>
+<a class="text" href="#">Oque é</a>
+</div>
+<div id="hamburger-icon" onclick="toggleMobileMenu(this)">
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+    <ul class="mobile-menu">
+        <a class="text1" href="index.html">Home</a>
+        <a class="text1" href="#">About Us</a>
+        <a class="text1" href="#">What Is This</a>
+    </ul>
+  </div>
+</header>
+
+<?php
+    $estrutura = <<<TEXT
+            <div class="container">
+            <h1>%s</h1>
+            <div class = "person">
+            <img src="%s">
+            <p>%s</p> 
+            </div>
+            <a href="github.com"><img class="git" src="git.svg""=></a>
+            <a href="linkedin.com"><img class="link" src="link.svg""=></a>
+        
+            </div>
+ 
+TEXT;
+    $perfil = [
+        ['Pessoa 1','perfil.png','Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, veritatis. Distinctio quo saepe ut repellat praesentium. Doloribus officia, accusamus eveniet inventore odio, cupiditate, ea quae explicabo velit laboriosam veniam maiores.
+        '],
+        ['Pessoa 2','perfil.png','Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, veritatis. Distinctio quo saepe ut repellat praesentium. Doloribus officia, accusamus eveniet inventore odio, cupiditate, ea quae explicabo velit laboriosam veniam maiores.
+        '],
+        ['Pessoa 3','perfil.png','Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, veritatis. Distinctio quo saepe ut repellat praesentium. Doloribus officia, accusamus eveniet inventore odio, cupiditate, ea quae explicabo velit laboriosam veniam maiores.
+        '],
+        ['Pessoa 4','perfil.png','Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, veritatis. Distinctio quo saepe ut repellat praesentium. Doloribus officia, accusamus eveniet inventore odio, cupiditate, ea quae explicabo velit laboriosam veniam maiores.
+        '],
+    ];
+    foreach ($perfil as $p) {
+        printf($estrutura, $p[0],$p[1],$p[2] );
+    }
+    ?>
+
+<div class="cor" id="cor"> <!--Div da cor tema-->
+        <button onclick="dark()">Dark</button> <!--Botão opção "mudar de cor tema"-->
+    </div>
+    <script src="public/js/script.js"></script>
+</body>
+</html>
