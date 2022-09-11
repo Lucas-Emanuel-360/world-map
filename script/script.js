4c0588eefd7c142b2b462de3896c4f9d78af938c
Clara Ribeiro <clara2020flor@gmail.com>
2022-09-11 16:13:09 -0300

    darkmode-incompleto

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function dark (){ //Botão cor tema
+    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='stylesheets/style-dark.css'/><button onclick='light()'>Light</button>")
+}
+function light (){
+    document.getElementById('cor').innerHTML = ("<link rel='stylesheet' type='text/css' href='stylesheets/style.css'/><button onclick='dark()'>Dark</button>")
+}
